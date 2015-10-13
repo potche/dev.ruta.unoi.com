23cf1282b7cf04b57bbd722456a4226339c342d3
julzone <julio@dsindigo.com>
2015-10-13 13:05:26 -0500

    #GAUSS-63 #Done #time 2h 10m #comment Se termina de implementar historia. Se realiza la consulta para traer respuestas de evaluacion de manera satisfactoria

---
+++ src/UNO/EvaluacionesBundle/Controller/Evaluaciones/AdminController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: julzone
+ * Date: 13/10/15
+ * Time: 11:35 AM
+ */
+
+namespace UNO\EvaluacionesBundle\Controller\Evaluaciones;
+
+
+class AdminController
+{
+
+}
