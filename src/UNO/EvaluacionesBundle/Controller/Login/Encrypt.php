0c48875fb279068fc3d2c230c50a2f0df9db6be6
Israel Reyes <israelr@dsindigo.com>
2015-10-14 17:51:25 -0500

    GAUSS-42 aplicando sonar...

--- src/UNO/EvaluacionesBundle/Controller/Login/Encrypt.php
+++ src/UNO/EvaluacionesBundle/Controller/Login/Encrypt.php
@@ -16,8 +16,8 @@ namespace UNO\EvaluacionesBundle\Controller\Login;
 DEFINE('ENCRYPTION_KEY', 'SomeRandomString');
 
 class Encrypt{
-    static $_key = '31CE5FE894ECF46134FC1AA365FF6F6F';
 
+    static $_key = '31CE5FE894ECF46134FC1AA365FF6F6F';
 
     /**
      * Devuelve una cadena encriptada.
@@ -26,6 +26,7 @@ class Encrypt{
      * @param string $encryptionKey clave opcional para la encriptación de la cadena
      *
      * @return string Devuelve la cadena encriptada
+     * $key = pack('H*', $encryptionKey);
      */
     public static function encrypt($pureString) {
         # --- ENCRYPTION ---
@@ -33,10 +34,7 @@ class Encrypt{
         # the key should be random binary, use scrypt, bcrypt or PBKDF2 to
         # convert a string into a key
         # key is specified using hexadecimal
-        //$key = pack('H*', $encryptionKey);
-        $ciphertext_base64 = trim(base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, static::$_key, $pureString, MCRYPT_MODE_ECB, mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB), MCRYPT_RAND))));
-        //echo "<br/>".$ciphertext_base64;
-        return $ciphertext_base64;
+        return trim(base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, static::$_key, $pureString, MCRYPT_MODE_ECB, mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB), MCRYPT_RAND))));
     }
 
     /**
@@ -46,6 +44,7 @@ class Encrypt{
      * @param string $encryptionKey clave opcional para la desencriptación de la cadena
      *
      * @return string Devuelve la cadena desencriptada
+     * $key = pack('H*', $encryptionKey);
      */
     public static function decrypt($encryptedString) {
         # === WARNING ===
@@ -58,9 +57,7 @@ class Encrypt{
         # the key should be random binary, use scrypt, bcrypt or PBKDF2 to
         # convert a string into a key
         # key is specified using hexadecimal
-        //$key = pack('H*', $encryptionKey);
 
         return trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, static::$_key, base64_decode($encryptedString), MCRYPT_MODE_ECB, mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB), MCRYPT_RAND)));
     }
-
 }
