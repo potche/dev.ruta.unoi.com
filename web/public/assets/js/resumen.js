c0271e489892982c7fd1d645060c330b05f80522
julzone <julio@dsindigo.com>
2015-10-14 14:40:27 -0500

    #GAUSS-62 #In Progress #time 4h #comment Se implementa seccion de ver preguntas y se comienza a implementar las graficas para la seccion de resumen

---
+++ web/public/assets/js/resumen.js
@@ -0,0 +1,17 @@
+var TablesDatatables = function() {
+
+    return {
+        init: function() {
+
+            App.datatables();
+
+            $('#tbl-respuestas').DataTable({
+                pageLength: 10,
+                lengthMenu: [[10, 20, 30, -1], [10, 20, 30, 'Ver Todas']]
+            });
+
+            $('.dataTables_filter input').attr('placeholder', 'Buscar');
+            $('#tbl-respuestas_info').hide();
+        }
+    };
+}();
