112e6489b57bca43f1f40c9cda1ec3ca39368ce3
julzone <julio@dsindigo.com>
2015-10-20 18:09:55 -0500

    #GAUSS-43 #In Progress #time 2h 30m #comment Avance en Desarrollo del controlador

--- src/UNO/EvaluacionesBundle/Controller/Evaluaciones/AdminController.php
+++ src/UNO/EvaluacionesBundle/Controller/Evaluaciones/AdminController.php
@@ -14,61 +14,60 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AdminController extends Controller
 {
-
     public function indexAction(Request $request){
 
-        $data = $this->getSurveyList();
-
+        $statsBySurvey = $this->getStatsBySurvey();
 
         return $this->render('UNOEvaluacionesBundle:Crear:menueval_admin.html.twig');
-
-
-
     }
 
-    private function getSurveyList(){
+    private function getStatsBySurvey(){
 
         $em = $this->getDoctrine()->getManager();
         $qb = $em->createQueryBuilder();
-        $surveyList = $qb->select(
-            "su.surveyid,su.title,
-        '' as perfiles,
-        '' as esperadas,
-        '' as respondidas,
-        '' as avance,
-        su.creationdate,
-        su.closingdate"
-        )->from('UNOEvaluacionesBundle:Survey', 'su')
-            ->orderBy('su.surveyid')
+        $expectedBySurvey = $qb->select("su.surveyid,'' as perfiles, '' as avance, su.creationdate, su.closingdate, count(distinct p.personid) as expectedNum")
+            ->from('UNOEvaluacionesBundle:Person', 'p')
+            ->innerJoin('UNOEvaluacionesBundle:Personschool','ps','WITH','p.personid = ps.personid')
+            ->innerJoin('UNOEvaluacionesBundle:Surveyxprofile','sxp', 'WITH','sxp.profileProfileid = ps.profileid AND ps.schoollevelid = sxp.schoollevelid')
+            ->innerJoin('UNOEvaluacionesBundle:Survey','su','WITH','su.surveyid = sxp.surveySurveyid')
+            ->where('sxp.surveySurveyid > 1')
+            ->andWhere('p.admin NOT IN (1)')
+            ->groupBy('sxp.surveySurveyid, perfiles, avance, su.creationdate, su.closingdate')
             ->getQuery()
             ->getResult();
 
-        return $surveyList;
-    }
-
-    private function fillDataArray($data){
-
-        foreach ($data as $row){
-
-            
-
-
-        }
-    }
-
-    private function getStatsBySurvey($data){
-
-        /*$em = $this->getDoctrine()->getManager();
         $qb = $em->createQueryBuilder();
-        $answeredBySurvey= $qb->select('sxp.surveySurveyid')
-            ->
-            ->from('UNOEvaluacionesBundle:Surveyxprofile', 'sxp')
-            ->innerJoin('UNOEvaluacionesBundle:Surveyxprofile', 'sxp', 'WITH', 'sxp.surveySurveyid = su.surveyid')
-            ->innerJoin('UNOEvaluacionesBundle:Profile','pr','WITH','pr.profileid = sxp.profileProfileid')
-            ->innerJoin('UNOEvaluacionesBundle:Schoollevel','sl','WITH','sl.schoollevelid = sxp.schoollevelid')
-            ->orderBy('su.surveyid')
+        $answeredBySurvey= $qb->select("su.surveyid, count(distinct ps.personid) as answeredNum")
+            ->from('UNOEvaluacionesBundle:Person', 'p')
+            ->innerJoin('UNOEvaluacionesBundle:Personschool','ps','WITH','p.personid = ps.personid')
+            ->innerJoin('UNOEvaluacionesBundle:Surveyxprofile','sxp', 'WITH','sxp.profileProfileid = ps.profileid AND ps.schoollevelid = sxp.schoollevelid')
+            ->innerJoin('UNOEvaluacionesBundle:Survey','su','WITH','su.surveyid = sxp.surveySurveyid')
+            ->innerJoin('UNOEvaluacionesBundle:Log','log','WITH','ps.personid = log.personPersonid AND sxp.surveySurveyid = log.surveySurveyid')
+            ->where('log.surveySurveyid > 1')
+            ->andWhere('p.admin NOT IN (1)')
+            ->andWhere('log.actionaction = 5')
+            ->groupBy('sxp.surveySurveyid')
             ->getQuery()
-            ->getResult();*/
+            ->getResult();
+
+        foreach($expectedBySurvey as $expectedElem) {
+
+            $avance = 0.0;
+            foreach ($answeredBySurvey as $answeredElement) {
+
+                if($expectedElem['surveyid'] == $answeredElement['surveyid']) {
+
+                    $avance  = ($answeredElement['answeredNum'] * 100 ) / $expectedElem['expectedNum'];
+                }
+            }
+
+            $expectedElem['avance'] = $avance;
+
+            /**
+             * ToDo: Query para obtener perfiles y niveles de evaluacion
+             */
+        }
+        return $expectedBySurvey;
     }
 
 }
