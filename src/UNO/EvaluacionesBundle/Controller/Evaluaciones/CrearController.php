65723019fb62dcdf3a1b6e9f07392620f9e90232
julzone <julio@dsindigo.com>
2015-10-26 14:08:36 -0600

    #GAUSS-48 #In Progress #time 4h #comment Manejo de eventos para agregar perfiles y preguntas, queda por hacer el manejo del sortable cuando se visita desde un movil

--- src/UNO/EvaluacionesBundle/Controller/Evaluaciones/CrearController.php
+++ src/UNO/EvaluacionesBundle/Controller/Evaluaciones/CrearController.php
@@ -15,7 +15,22 @@ use Symfony\Component\HttpFoundation\Request;
 
 class CrearController extends Controller {
 
+    /**
+     *
+     * Método principal del controlador para crear evaluaciones
+     *
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     * @throws AccessDeniedHttpException
+     * @author julio
+     * @version 0.2.0
+     */
+
     public function indexAction(Request $request){
+
+        /**
+         * Manejo de sesión
+         */
 
         $session = $request->getSession();
 
@@ -29,9 +44,21 @@ class CrearController extends Controller {
             throw new AccessDeniedHttpException('No estás autorizado para realizar esta acción');
         }
 
+
+
+
         return $this->render('UNOEvaluacionesBundle:Crear:crear.html.twig');
 
     }
+
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     * @throws InternalErrorException
+     *
+     * Método invocado para almacenar una nueva evaluación en la bd
+     *
+     */
 
     public function saveAction(Request $request){
 
@@ -40,11 +67,32 @@ class CrearController extends Controller {
             throw new InternalErrorException('Ha ocurrido un error al procesar esta petición');
         }
 
+        var_dump($_POST['eval']);
+        exit();
+
         /**
          * ToDo: implementar lógica para almacenar evaluación a partir de la petición
          */
 
         return $this->redirectToRoute('evaluaciones');
+    }
+
+    private function getCategories(){
+
+    }
+
+    private function getQuestions(){
+
+
+    }
+
+    private function getLevels(){
+
+    }
+
+    private function getProfiles(){
+
+
     }
 
 }
---
+++ web/public/assets/js/uiDraggable.js
@@ -0,0 +1,20 @@
+
+var UiDraggable = function() {
+
+    return {
+        init: function() {
+            /* Initialize draggable and sortable blocks, check out more examples at https://jqueryui.com/sortable/ */
+            $('.draggable-blocks').sortable({
+                connectWith: '.block',
+                items: '.block',
+                opacity: 0.75,
+                handle: '.block-title',
+                placeholder: 'draggable-placeholder',
+                tolerance: 'pointer',
+                start: function(e, ui){
+                    ui.placeholder.css('height', ui.item.outerHeight());
+                }
+            });
+        }
+    };
+}();
