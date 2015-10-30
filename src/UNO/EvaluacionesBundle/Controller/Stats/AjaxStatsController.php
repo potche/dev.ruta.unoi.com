38e1f4b027fbd87f00e0aa603130ccb0cfbb21f6
Israel Reyes <israelr@dsindigo.com>
2015-10-30 14:55:25 -0600

    #GAUSS-58 #time 4h #Done

---
+++ src/UNO/EvaluacionesBundle/Controller/Stats/AjaxStatsController.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: isra
+ * Date: 30/10/15
+ * Time: 10:17 AM
+ */
+
+namespace UNO\EvaluacionesBundle\Controller\Stats;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use UNO\EvaluacionesBundle\Entity\Personschool;
+use UNO\EvaluacionesBundle\Entity\Optionxquestion;
+
+class AjaxStatsController extends Controller{
+
+    /**
+     * @Route("/ajax/stats")
+     *
+     *
+     */
+    public function indexAction(Request $request){
+
+        $session = $request->getSession();
+        $session->start();
+        if ($request->getMethod() == 'POST') {
+            $personId = $request->get('personId');
+            $title = $request->get('title');
+            $survey = $this->getSurveyResponse($personId, $title);
+            $tableSurvey = $this->creaTable($survey, $title);
+            return new response($tableSurvey);
+        }else{
+            return new response($request->getMethod());
+        }
+    }
+
+    private function getSurveyResponse($personId, $title) {
+
+        $em = $this->getDoctrine()->getManager();
+        $qb = $em->createQueryBuilder();
+
+        $_surveyResponse = $qb->select("QS.order, Q.question, A.answer, A.comment")
+            ->from('UNOEvaluacionesBundle:Answer','A')
+            ->innerJoin('UNOEvaluacionesBundle:Optionxquestion','OQ', 'WITH', 'A.optionxquestion = OQ.optionxquestionId')
+            ->innerJoin('UNOEvaluacionesBundle:Questionxsurvey','QS', 'WITH', 'OQ.questionxsurvey = QS.questionxsurveyId')
+            ->innerJoin('UNOEvaluacionesBundle:Question','Q', 'WITH', 'QS.questionQuestionid = Q.questionid')
+            ->innerJoin('UNOEvaluacionesBundle:Survey','S', 'WITH', 'QS.surveySurveyid = S.surveyid')
+            ->where('A.personPersonid = :personid')
+            ->andWhere('S.title = :title')
+            ->setParameters(array('personid' => $personId, 'title' => $title))
+            ->getQuery()
+            ->getResult();
+        return ($_surveyResponse);
+    }
+
+    private function creaTable($survey,$title){
+        $table ='
+        <div class="row text-center ">
+            <div class="block-content text-center">
+                <div class="col-sm-12 col-md-12 col-lg-12">
+                    <div class="block">
+                        <div class="block-title">
+                            <h2>Evaluación <strong>'.$title.'</strong></h2>
+                        </div>
+                        <div class="table-responsive">
+                            <p>Detalle de la Evaluación.</p>
+                            <table id="userEva-datatable" class="table table-vcenter table-condensed table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th class="text-center">Orden</th>
+                                        <th class="text-center">Preunta</th>
+                                        <th class="text-center">Respuesta</th>
+                                        <th class="text-center">Comemntario</th>
+                                    </tr>
+                                </thead>
+                                <tbody>';
+        foreach($survey as $value){
+            $table .= '
+                                    <tr>
+                                        <td>'.$value['order'].'</td>
+                                        <td>'.$value['question'].'</td>
+                                        <td>'.$value['answer'].'</td>
+                                        <td>'.$value['comment'].'</td>
+                                    </tr>';
+        }
+
+        $table .='
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>';
+
+        return $table;
+    }
+
+}
