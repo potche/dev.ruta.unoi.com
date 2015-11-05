cdd8980bc9277e375097a50cfd0d0950f2df5a7b
julzone <julio@dsindigo.com>
2015-11-04 18:05:37 -0600

    #GAUSS-85 #Done #time 1d 4h #comment Se crea script validado para ser ejecutado en BD productiva una vez que se haga despliegue

---
+++ src/UNO/EvaluacionesBundle/Command/CleanDatabaseCommand.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace UNO\EvaluacionesBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CleanDatabaseCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('db:removedups')
+            ->setDescription('Remove duplicates')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $em = $this->getContainer()->get('doctrine')->getEntityManager();
+        $connection = $em->getConnection();
+
+        $output->writeln("Obteniendo preguntas duplicadas");
+
+        $q = "SELECT CONCAT(A.Person_personId,'-',surveyId,'-',questionId) as val, A.answerId
+            FROM
+                Answer A
+            INNER JOIN
+	            OptionXQuestion OXQ
+                ON A.OptionXQuestion_id = OXQ.OptionXQuestion_id
+            INNER JOIN
+	        QuestionXSurvey QXS
+                ON QXS.QuestionXSurvey_id = OXQ.QuestionXSurvey_id
+            INNER JOIN Survey S
+	            ON S.surveyId = QXS.Survey_surveyId
+            INNER JOIN Question Q
+	            ON Q.questionId = QXS.Question_questionId
+            GROUP BY questionId, surveyId, Person_personId, AnswerId";
+
+        $statement = $connection->prepare($q);
+        $statement->execute();
+        $results = $statement->fetchAll();
+
+        $filtered = array();
+
+        foreach ($results as $r) {
+
+            $log_info = explode('-',$r['val']);
+
+            if(!array_key_exists($r['val'],$filtered)) {
+
+                $filtered[$r['val']] = array();
+            }
+
+            array_push($filtered[$r['val']],array(
+                'answer' => $r['answerId'],
+                'person' => $log_info[0],
+                'survey' => $log_info[1]
+            ));
+        }
+
+        $output->writeln("Eliminando preguntas duplicadas y entradas del log");
+        $i = 0;
+
+        foreach ($filtered as $f) {
+
+            if(count($f) > 1){
+
+                foreach($f as $aid){
+
+                    $output->writeln(" ");
+                    $output->writeln("Eliminando:\tPregunta: ".$aid['answer']."\tPersona: ".$aid['person']."\tEvaluacion: ".$aid['survey']);
+
+                    $q = "DELETE FROM Answer WHERE Answer.AnswerId = :answer";
+                    $statement = $connection->prepare($q);
+                    $statement->bindValue('answer',$aid['answer']);
+                    $statement->execute();
+
+                    $q = "DELETE FROM Log WHERE (Person_personId = :person AND Survey_surveyId = :survey)";
+                    $statement = $connection->prepare($q);
+                    $statement->bindValue('person',$aid['person']);
+                    $statement->bindValue('survey',$aid['survey']);
+                    $statement->execute();
+
+                    $i++;
+                }
+            }
+
+            else{
+
+                $output->writeln(" ");
+                $output->writeln("No se detectaron preguntas duplicadas, sé feliz! (por hoy)");
+                exit();
+            }
+        }
+        $output->writeln("Finalizado de eliminar ".$i." duplicados y limpiar logs");
+    }
+}
