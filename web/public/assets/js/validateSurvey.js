4b4eaf8c146f83191b2b65914feca611e0616aac
israelrey <israelr@dsindigo.com>
2015-11-24 17:55:40 -0600

    #GAUSS-98 #time 6h #In Progress

---
+++ web/public/assets/js/validateSurvey.js
@@ -0,0 +1,51 @@
+/*
+ *  Document   : validateSurvey.js
+ *  Author     : isra
+ *  Description: valida el filtro por Escuela
+ */
+
+var ValidateSurvey = function() {
+
+
+    return {
+        init: function(surveyTypeheadData) {
+
+            jQuery.validator.addMethod("valSurvey", function(value, element){
+
+                    if(surveyTypeheadData.indexOf(value) != -1){
+                        return true
+                    }
+                    return false
+
+            }, '<div class="text-danger"><i class="fa fa-times"></i> Ingrese una Evaluacion Valida</div>');
+
+            /*
+             *  Jquery Validation, Check out more examples and documentation at https://github.com/jzaefferer/jquery-validation
+             */
+
+            /* Login form - Initialize Validation */
+            $('#surveyFrm').validate({
+                onfocusout: false,
+                onkeyup: false,
+                onclick: false,
+
+                errorClass: 'help-block animation-slideDown', // You can change the animation class for a different entrance animation - check animations page
+                errorElement: 'div',
+                rules: {
+                    surveyIdFrm: {
+                        required: true,
+                        valSurvey: true
+                    }
+                },
+                messages: {
+                    surveyIdFrm: {
+                        required: '<div class="text-danger"><i class="fa fa-times"></i> Ingresa una Evalacion</div>'
+                    }
+                },
+                submitHandler: function(form) {
+                    return true;
+                }
+            });
+        }
+    };
+}();
