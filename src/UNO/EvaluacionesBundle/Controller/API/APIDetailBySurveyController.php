66858bed98b822daf99a5a97b4380be3f15c681d
israel reyes <israelr@dsindigo.com>
2016-04-29 14:58:27 -0500

    #GAUSS-153 #time 4h #In Progress

--- src/UNO/EvaluacionesBundle/Controller/API/APIDetailBySurveyController.php
+++ src/UNO/EvaluacionesBundle/Controller/API/APIDetailBySurveyController.php
@@ -105,4 +105,100 @@ class APIDetailBySurveyController extends Controller{
 
         return $parsed;
     }
+
+    /**
+     *
+     * Esta función despacha a la ruta /api/v0/detail/{surveyId}
+     *
+     * @param Request $request
+     * @return JsonResponse
+     * @throws \Exception
+     */
+
+    public function detailForAdminGrlAction(Request $request){
+
+        $params = $request->attributes->all()['_route_params'];
+        $response = new JsonResponse();
+        $response->setData($this->getCountResponse($params));
+        return $response;
+    }
+
+    private function getCountResponse($params){
+
+        $em = $this->getDoctrine()->getManager();
+        $qb = $em->createQuerybuilder();
+
+        $all = $qb->select("su.surveyid as id, su.title as titulo, qxs.order as ord, q.question as pregunta, o.option as opcion, COUNT(p.personid) as suma")
+            ->from('UNOEvaluacionesBundle:Survey','su')
+            ->leftJoin('UNOEvaluacionesBundle:Questionxsurvey','qxs','WITH','su.surveyid = qxs.surveySurveyid')
+            ->leftJoin('UNOEvaluacionesBundle:Question','q','WITH','q.questionid = qxs.questionQuestionid')
+            ->leftJoin('UNOEvaluacionesBundle:Optionxquestion','oxq','WITH','oxq.questionxsurvey = qxs.questionxsurveyId')
+            ->leftJoin('UNOEvaluacionesBundle:Option','o','WITH','o.optionid = oxq.optionOptionid')
+            ->leftJoin('UNOEvaluacionesBundle:Answer','ans','WITH','ans.optionxquestion = oxq.optionxquestionId')
+            ->innerJoin('UNOEvaluacionesBundle:Person','p','WITH','p.personid = ans.personPersonid')
+            ->innerJoin('UNOEvaluacionesBundle:Personschool','ps','WITH','ps.personid = p.personid')
+            ->innerJoin('UNOEvaluacionesBundle:Surveyxprofile','sxp','WITH','ps.profileid = sxp.profileProfileid AND ps.schoollevelid = sxp.schoollevelid AND sxp.surveySurveyid = su.surveyid')
+            ->innerJoin('UNOEvaluacionesBundle:Log','l', 'WITH','l.personPersonid = ps.personid AND l.surveySurveyid = su.surveyid')
+            ->innerJoin('UNOEvaluacionesBundle:Action','a', 'WITH','a.idaction = l.actionaction AND a.idaction = 4')
+            ->where('su.surveyid = '.$params['surveyId'])
+            ->groupBy('ord, pregunta, opcion')
+            ->orderBy('ord, opcion')
+            ->getQuery()
+            ->getResult();
+
+        return $all ? $this->parseCountResults($all) : APIUtils::getErrorResponse('404');
+    }
+
+    /**
+     *
+     * Parseo de resultados de query
+     *
+     * @param $results
+     * @return array
+     */
+
+    private function parseCountResults($results){
+
+        $options = ['Sí','No','No sé'];
+        $numQuestions = array_unique(array_column($results,'ord'));
+        $parsed = array(
+            'id'=>$results[0]['id'],
+            'titulo'=>$results[0]['titulo'],
+            'preguntas'=>array()
+        );
+
+        foreach($numQuestions as $num){
+
+            $numRes = array_filter($results, function($ar) use($num){ return ($ar['ord'] == $num); });
+            $orden = array_unique(array_column($numRes,'ord'));
+            $pregunta = array_unique(array_column($numRes,'pregunta'));
+
+            $pregunta = array(
+                'orden' =>  $orden[0],
+                'pregunta' =>  $pregunta[0],
+                'opciones' => array()
+            );
+
+            $p = array_push($parsed['preguntas'],$pregunta);
+
+            foreach($options as $o){
+
+                $optRes = array_filter($numRes, function($ar) use($o){ return ($ar['opcion'] == $o); });
+                $total = array();
+
+                foreach ($optRes as $per){
+                    $total = $per['suma'];
+                }
+
+                $opcion = array(
+                    'opcion' => $o,
+                    'total' => $total
+                );
+
+                array_push($parsed['preguntas'][$p-1]['opciones'],$opcion);
+            }
+        }
+
+        return $parsed;
+    }
 }
