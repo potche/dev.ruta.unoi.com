fad4d9d065ea3eedb461809a7665429ac6d1167d
israel reyes <israelr@dsindigo.com>
2016-05-04 18:01:23 -0500

    #GAUSS-159 #time 1h #Done

--- web/public/assets/js/resumen.js
+++ web/public/assets/js/resumen.js
@@ -18,3 +18,60 @@ var TablesDatatables = function() {
         }
     };
 }();
+
+function getResume(surveyId, personId){
+    $.get( "/api/v0/result/survey/"+surveyId+"/person/"+personId)
+        .done(function( data ) {
+
+            console.log(data.persons[0].surveys[0].questions)
+            $('#table-tareas').html(createDataTableRes(data.persons[0].surveys[0].questions));
+        })
+        .fail(function(error) {
+            console.log( error );
+        })
+        .always(function() {
+            console.log("valid Assigned finished")
+        });
+}
+
+function getSurveyId(uri){
+    var usriArr = uri.split('/');
+    var zise = usriArr.length;
+
+    return usriArr[zise-1];
+}
+
+function createDataTableRes(dat){
+    var dataTable =
+        '<div class="table-responsive">'+
+            '<table id="tbl-datatable-tarea" class="table table-bordered">'+
+                '<thead>'+
+                    '<tr>'+
+                        '<th class="hidden-sm hidden-xs"><b>#</b></th>'+
+                        '<th class="text-center"><b>Pregunta</b></th>'+
+                        '<th class="text-center">'+
+                            '<span class="visible-lg-inline visible-md-inline visible-sm-inline hidden-xs"><b>Respuesta</b></span>'+
+                            '<span class="visible-xs-inline"><b><i class="fa fa-pencil-square-o"></i></b></span>'+
+                        '</th>'+
+                        '<th class="text-center">'+
+                            '<span class="visible-lg-inline visible-md-inline visible-sm-inline hidden-xs"><b>Comentario</b></span>'+
+                            '<span class="visible-xs-inline"><b><i class="fa fa-commenting-o"></i></b></span>'+
+                        '</th>'+
+                    '</tr>'+
+                '</thead>'+
+                '<tbody>';
+
+
+    $.each(dat, function (key, value) {
+        dataTable += '';
+        console.log(value);
+    });
+
+
+    dataTable +=
+                '</tbody>'+
+            '</table>'+
+        '</div>';
+
+    return dataTable;
+}
